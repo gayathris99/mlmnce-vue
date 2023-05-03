3558027dd7f365c9a7e92302f947d090c34e8155
gayathris99 <gayathri11499@gmail.com>
2023-05-03 15:44:56 +0530

    Affilaition Order

--- src/pages/Academia/AffiliationOrder.vue
+++ src/pages/Academia/AffiliationOrder.vue
@@ -1,3 +1,16 @@
 <template>
-  <div>Affiliation Order</div>
+  <div class="q-pa-md">
+    <div style="color:#303a57" class="fw-500 fs-24">AFFILIATION ORDER</div>
+    <div class="row  align-center items-center q-mt-md">
+      <img src="https://i.ibb.co/x58TFVq/Affiliation-Order.png" alt="MLMNCE">
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+img {
+  max-width: 80vw;
+  min-width: 50vw;
+  max-height: 100vh;
+}
+</style>
