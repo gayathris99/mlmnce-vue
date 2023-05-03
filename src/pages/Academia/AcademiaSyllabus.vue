6beda9522223224ab3b56aaca1f45b43e762183f
gayathris99 <gayathri11499@gmail.com>
2023-05-03 16:00:38 +0530

    Syllabus

--- src/pages/Academia/AcademiaSyllabus.vue
+++ src/pages/Academia/AcademiaSyllabus.vue
@@ -1,3 +1,36 @@
 <template>
-  <div>Syllabus</div>
+  <div class="q-pa-lg">
+    <div style="color:#303a57" class="fw-500 fs-24">SYLLABUS</div>
+    <div class="row align-center items-center justify-start q-mt-md q-gutter-md">
+      <a href="https://portfolio-platform.s3.ap-south-1.amazonaws.com/media/public/mlmncecollege/pdf/syllabus/B.Ed.%2C+Syllabus+2022-23.pdf" target="_blank">
+        <div class="col-4 column items-center ">
+          <img src="https://portfolio-platform.s3.ap-south-1.amazonaws.com/media/public/mlmncecollege/helper-images/PdfLogo.png" alt="">
+          <div class="fs-12 q-mt-md">B.Ed 2022-23</div>
+        </div>
+      </a>
+      <a href="https://portfolio-platform.s3.ap-south-1.amazonaws.com/media/public/mlmncecollege/pdf/syllabus/2+Year+B.Ed+Syllabus+(Final+Print).pdf" target="_blank">
+        <div class="col-4 column items-center">
+          <img src="https://portfolio-platform.s3.ap-south-1.amazonaws.com/media/public/mlmncecollege/helper-images/PdfLogo.png" alt="">
+          <div class="fs-12 q-mt-md">2nd Year B.Ed 2022-23</div>
+        </div>
+      </a>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'AcademiaSyllabus'
+}
+</script>
+
+<style lang="scss" scoped>
+img {
+  width: 75px;
+  height: 75px;
+}
+a {
+  text-decoration: none;
+  color: $dark;
+}
+</style>
