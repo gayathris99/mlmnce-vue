18f2735e6e8e6ec224b09976e1e55b7fc6b0bff0
gayathris99 <gayathri11499@gmail.com>
2023-05-04 12:19:49 +0530

    IQAC Compositions

--- src/pages/NAAC/IQACCompositions.vue
+++ src/pages/NAAC/IQACCompositions.vue
@@ -1,3 +1,44 @@
 <template>
-  iqac Compositions
+  <div class="q-pa-lg">
+    <div style="color:#303a57" class="fw-500 fs-24">IQAC COMPOSITIONS</div>
+    <div class="row align-center items-center justify-start q-mt-md q-gutter-md">
+      <div v-for="(composition, key) in compositions" :key="key">
+        <a :href="composition.link" target="_blank">
+        <div class="col-2  column items-center ">
+          <img src="https://portfolio-platform.s3.ap-south-1.amazonaws.com/media/public/mlmncecollege/helper-images/PdfLogo.png" alt="">
+          <div class="fs-12 q-mt-md">{{composition.name}}</div>
+        </div>
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'IQACCompositions',
+  data () {
+    return {
+      compositions: [
+        {link: 'https://portfolio-platform.s3.ap-south-1.amazonaws.com/media/public/mlmncecollege/pdf/iqaccomposition/IQAC+Compositin+2016-17.pdf', name: 'IQAC Composition 2016-17'},
+        {link: 'https://portfolio-platform.s3.ap-south-1.amazonaws.com/media/public/mlmncecollege/pdf/iqaccomposition/IQAC+Compositin+2017-18.pdf', name: 'IQAC Composition 2017-18'},
+        {link: 'https://portfolio-platform.s3.ap-south-1.amazonaws.com/media/public/mlmncecollege/pdf/iqaccomposition/IQAC+Compositin+2018-19.pdf', name: 'IQAC Composition 2018-19'},
+        {link: 'https://portfolio-platform.s3.ap-south-1.amazonaws.com/media/public/mlmncecollege/pdf/iqaccomposition/IQAC+Compositin+2019-20.pdf', name: 'IQAC Composition 2019-20'},
+        {link: 'https://portfolio-platform.s3.ap-south-1.amazonaws.com/media/public/mlmncecollege/pdf/iqaccomposition/IQAC+Compositin+2020-21.pdf', name: 'IQAC Composition 2020-21'},
+        {link: 'https://portfolio-platform.s3.ap-south-1.amazonaws.com/media/public/mlmncecollege/pdf/iqaccomposition/IQAC+Compositin+2021-22.pdf', name: 'IQAC Composition 2021-22'},
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+img {
+  width: 75px;
+  height: 75px;
+}
+a {
+  text-decoration: none;
+  color: $dark;
+}
+</style>
--- src/pages/NAAC/NAACCertificate.vue
+++ src/pages/NAAC/NAACCertificate.vue
@@ -1,3 +1,41 @@
 <template>
-  naac certificate
+  <div class="q-pa-lg">
+    <div style="color:#303a57" class="fw-500 fs-24">NAAC CERTIFICATE</div>
+    <!-- <q-btn label="download" @click="downloadItem"></q-btn> -->
+  </div>
 </template>
+
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      // pdfLink: require("../../assets/NAACCertificate.pdf")
+    }
+  },
+  methods: {
+    downloadItem () {
+    //   axios({
+    //     url: this.pdfLink, // File URL Goes Here
+    //     method: 'GET',
+    //     responseType: 'blob',
+    // }).then((res) => {
+    //                  var FILE = window.URL.createObjectURL(new Blob([res.data]));
+
+    //                  var docUrl = document.createElement('x');
+    //                  docUrl.href = FILE;
+    //                  docUrl.setAttribute('download', 'file.pdf');
+    //                  document.body.appendChild(docUrl);
+    //                  docUrl.click();
+    //             });
+      var FILE = window.URL.createObjectURL(new Blob([this.pdfLink]));
+
+      var docUrl = document.createElement('x');
+      docUrl.href = FILE;
+      docUrl.setAttribute('download', 'file.pdf');
+      document.body.appendChild(docUrl);
+      docUrl.click();
+  }
+}
+}
+</script>
