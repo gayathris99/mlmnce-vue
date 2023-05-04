4de239998e2005019d0e2773cb702f8c8b688de3
gayathris99 <gayathri11499@gmail.com>
2023-05-04 12:22:40 +0530

    AQAR

--- src/pages/NAAC/AQARComponent.vue
+++ src/pages/NAAC/AQARComponent.vue
@@ -1,3 +1,30 @@
 <template>
-  AQARComponent.vue
+  <div class="q-pa-lg">
+    <div style="color:#303a57" class="fw-500 fs-24">AQAR</div>
+    <div class="row align-center items-center justify-start q-mt-md q-gutter-md">
+      <a href="https://portfolio-platform.s3.ap-south-1.amazonaws.com/media/public/mlmncecollege/pdf/aqar/AQAR+2016.pdf" target="_blank">
+      <div class="col-2  column items-center ">
+        <img src="https://portfolio-platform.s3.ap-south-1.amazonaws.com/media/public/mlmncecollege/helper-images/PdfLogo.png" alt="">
+        <div class="fs-12 q-mt-md">AQAR 2016-17</div>
+      </div>
+      </a>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'AQAR'
+}
+</script>
+
+<style lang="scss" scoped>
+img {
+  width: 75px;
+  height: 75px;
+}
+a {
+  text-decoration: none;
+  color: $dark;
+}
+</style>
