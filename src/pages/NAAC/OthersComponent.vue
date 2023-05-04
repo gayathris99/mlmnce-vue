d5aa65b3b18e4463b94013d3e819290bc3dcbbfe
gayathris99 <gayathri11499@gmail.com>
2023-05-04 12:30:28 +0530

    Student Survey and others

--- src/pages/NAAC/OthersComponent.vue
+++ src/pages/NAAC/OthersComponent.vue
@@ -1,3 +1,36 @@
 <template>
-  OthersComponent.vue
+  <div class="q-pa-lg">
+    <div style="color:#303a57" class="fw-500 fs-24">OTHERS</div>
+    <div class="row align-center items-center justify-start q-mt-md q-gutter-md">
+      <a href="https://portfolio-platform.s3.ap-south-1.amazonaws.com/media/public/mlmncecollege/pdf/others/Self+Declaration.pdf" target="_blank">
+      <div class="col-2  column items-center ">
+        <img src="https://portfolio-platform.s3.ap-south-1.amazonaws.com/media/public/mlmncecollege/helper-images/PdfLogo.png" alt="">
+        <div class="fs-12 q-mt-md">Self Declaration</div>
+      </div>
+      </a>
+      <a href="https://portfolio-platform.s3.ap-south-1.amazonaws.com/media/public/mlmncecollege/pdf/others/undertaking.pdf" target="_blank">
+      <div class="col-2  column items-center ">
+        <img src="https://portfolio-platform.s3.ap-south-1.amazonaws.com/media/public/mlmncecollege/helper-images/PdfLogo.png" alt="">
+        <div class="fs-12 q-mt-md">Undertaking</div>
+      </div>
+      </a>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'OthersComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+img {
+  width: 75px;
+  height: 75px;
+}
+a {
+  text-decoration: none;
+  color: $dark;
+}
+</style>
--- src/pages/NAAC/StudentSurvey.vue
+++ src/pages/NAAC/StudentSurvey.vue
@@ -1,3 +1,30 @@
 <template>
-  StudentSurvey.vue
+  <div class="q-pa-lg">
+    <div style="color:#303a57" class="fw-500 fs-24">STUDENT SATISFACTION SURVEY</div>
+    <div class="row align-center items-center justify-start q-mt-md q-gutter-md">
+      <a href="https://portfolio-platform.s3.ap-south-1.amazonaws.com/media/public/mlmncecollege/pdf/studentsurvey/SSS++2017-18.pdf" target="_blank">
+      <div class="col-2  column items-center ">
+        <img src="https://portfolio-platform.s3.ap-south-1.amazonaws.com/media/public/mlmncecollege/helper-images/PdfLogo.png" alt="">
+        <div class="fs-12 q-mt-md">SSS 2017-18</div>
+      </div>
+      </a>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'StudentSurvey'
+}
+</script>
+
+<style lang="scss" scoped>
+img {
+  width: 75px;
+  height: 75px;
+}
+a {
+  text-decoration: none;
+  color: $dark;
+}
+</style>
